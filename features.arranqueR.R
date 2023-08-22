91a44b37e6ff873081e9034738619ed284186cc0
Joel Hu <a20196510@pucp.edu.pe>
2023-08-22 16:43:06 -0500

    Add files via upload

---
+++ features.arranqueR.R
@@ -0,0 +1,11 @@
+# Para acceder al archivo .Rprofile, primero instalar `usethis` 
+# Luego correr: `usethis::edit_r_profile()`
+
+# Ya en .Rprofile, colocar esto: 
+
+# print("Hola mundo")
+
+# Otra opción es...
+# para ello, instalar `cowsay`
+cowsay::say("Hola Mundo", 
+            by="cat")
